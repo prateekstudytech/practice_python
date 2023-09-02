7b6b62b797a6903682b779e26736d55bea8b6d47
prateekstudytech <prateekstudytech@gmail.com>
2023-09-02 17:39:23 +0530

    Commit changes to Dog Class

--- oops/dog.py
+++ oops/dog.py
@@ -1,16 +1,55 @@
 class Dog:
+    """
+    This is a class for defining Dogs.
+
+
+
+    Returns:
+        Object of type Dog
+    """
 
     species = "mammal"
 
     def __init__(self, name, age):
+        """
+        __init__ The constructor for Dog class
+
+        Arguments:
+            name -- Name of the dog
+            age -- Age of the dog
+        """
         self.name = name
         self.age = age
+    
+    def description(self):
+        """
+        description function to return description of Dog
+
+        Returns: 
+            string: Dog is x years old.
+        """
+        return f"{self.name} is {self.age} years old."
+    
+    def speak(self, sound):
+        """
+        speak function to return greet sound by the dog
+
+        Arguments:
+            sound:str -- sound of the dog
+
+        Returns:
+            str: -- greet of the dog
+        """
+        return f"{self.name} says {sound}"
+    
+    def birthday(self):
+        """
+        birthday Increment age on the birthdays
+        """
+        self.age += 1
 
 if __name__ == "__main__":
     philo = Dog("Philo", 5)
     mikey = Dog("Mikey", 6)
 
-    print(f"{philo.name} is {philo.age} and {mikey.name} is {mikey.age}")
-
-    if philo.species == "mammal":
-        print(f"{philo.name} is a {philo.species}")
+    print(mikey.description())
